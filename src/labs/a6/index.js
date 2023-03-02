6a6448b76ee67a10e704076924268d0f964150e3
Vineeth <vineeth122@yahoo.co.in>
2023-03-01 23:58:58 -0500

    Assignment 6 inprogress

---
+++ src/labs/a6/index.js
@@ -0,0 +1,20 @@
+// import {Link} from "react-router-dom";
+import Nav from "../../nav";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+// import todo from "./todo";
+import TodoList from "./todo/todo-list";
+function Assignment6() {
+    return (
+        <div>
+            <Nav/>
+            <h1>Assignment 6</h1>
+            <ConditionalOutput/>
+            <Classes/>
+            <Styles/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Assignment6;
