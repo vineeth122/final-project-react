09873cae268a185669cf154362d449890bb0a1d7
Vineeth <vineeth122@yahoo.co.in>
2023-04-02 19:11:40 -0400

    Assignment 8 and Tuiter Updated with REACT_APP_API_BASE

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const TUITS_API = 'https://tuiter-node-server-app-nats.onrender.com/api/tuits';
+const TUITS_API = process.env.REACT_APP_API_BASE;;
 export const createTuit = async (tuit) => {
         const response = await axios.post(TUITS_API, tuit);
         return response.data;
